7447e9a8c31ecab64c8dbe0b2744ab79b4df3fc0
lauraesilva-design <laura.e.silva@aluno.senai.br>
2025-10-02 16:44:13 -0300

    Add files via upload

--- Calculos/logica.js
+++ Calculos/logica.js
@@ -10,9 +10,25 @@ function soma() {
 }
 
 function sub() {
-    var valor1 = document.getElementById("v1").valueAsNumber
-    var valor2 = document.getElementById("v2").valueAsNumber
-    var resposta = document.getElementById("respSub")
+    var valor1 = parseFloat(document.getElementById("v1").value)
+    var valor2 = parseFloat(document.getElementById("v2").value)
+    document.getElementById("respSub").textContent = valor1 - valor2
 
-    resposta.textContent = valor1 - valor2
+}
+function multi(){
+    var number1= parseFloat(document.getElementById("v3").value)
+    var number2= parseFloat(document.getElementById("v4").value)
+     document.getElementById("respMulti").textContent = number1 * number2
+}
+function div(){
+    var chuva1= parseFloat(document.getElementById("v5").value)
+    var chuva2= parseFloat(document.getElementById("v6").value)
+    var respo=document.getElementById("respDiv").textContent = chuva1 / chuva2
+
+    if (chuva2 !== 0){
+        resp.innerHTML=(chuva1/chuva2).toFixed(2)
+    }else{
+        resp.innerHTML= "Não se divide por 0"
+    }
+        
 }
