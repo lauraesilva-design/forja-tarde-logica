92ecdd52189b60190f604b057f653a5368a23abb
lauraesilva-design <laura.e.silva@aluno.senai.br>
2025-10-20 16:25:45 -0300

    Add files via upload

---
+++ Revisao/script.js
@@ -0,0 +1,38 @@
+ // console.log("Alunos maravilhosos")
+
+ //variaveis
+
+ //let nome = "matheus"
+ //nome = "batata"
+ //console.log.(nome)
+
+ //var fruta= 3
+ //var fruta ="abacate"
+ //fruta= "outra"
+ //console.log(typeof fruta, fruta)
+
+ //const tem = true
+ //tem= "andre"
+ //console.log(typeof tem, tem)
+
+ //var preco = 25
+ //var quantidade = 5
+ //var total = preco * quantidade
+ //console.log(total)
+
+ //concatenar Strings e variaveis
+ //var resposta = "este preco" + " " + preco + " " + "as vezes este valor" + " " + quantidade + " " + "é resultante = " + total
+
+//template String
+//var resposta = 'Este preco ${preco} vezes este valor ${quntidade} = ${total}'
+//console.log(resposta)
+
+//var nome = "laura"
+//var idade = 15
+//var tudoJunto1 =  "Meu nome é " + nome + " e eu tenho " + idade + " anos." 
+//var tudoJunto = Meu nome é ${nome} e eu tenho ${idade} anos.
+
+//console.log(tudoJunto)
+
+
+
---
+++ treinos/script.js
@@ -0,0 +1,37 @@
+//var fruta = "abacaxi"
+
+//console.log(fruta)
+
+//var nome= "laura"
+//nome = "batata"
+//console.log(nome)
+
+//let lala
+//lala = "cadeira"
+//console.log(lala)
+
+ //var numero= 2
+ //numero= "vanessa"
+ //console.log(typeof familia, familia)
+
+//const frase ="it's time to rock"
+//console.log(frase)
+
+//console.log(typeof fruta, typeof nome, typeof tri, typeof n )
+
+//var numero= "5"
+//console.log(numero, typeof numero)
+
+var teste= "8"
+teste = 8
+console.log(teste)
+
+//funcoes
+function testando(parametro1, parametro2){
+    var a = parametro1
+    var b =parametro2
+    return a + b
+}
+console.log(testando(18, 2))
+
+
